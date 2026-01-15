9ea3d3ba8da6e1a909e7daa982503d5ba41e7e1c
serghd <serghd.box@gmail.com>
2026-01-15 15:31:17 +0200

    chore: add Tuples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 mod option;
+mod tuples;
+
+use crate::tuples::{tuple_destructuring, tuple_print};
 
 fn main() {
-    // #1. Create and display object using Option
-    option::create_and_display_object();
+    // // #1. Create and display object using Option
+    // option::create_and_display_object();
+
+    // // #2. Tuple Operations
+    // tuple_print();
+    // tuple_destructuring();
+
+    let my_number: u8 = 10;
+    match my_number {
+       number @ 10 => {
+           println!("result: {}", number)
+       },
+        _ => {
+            
+        }
+    };
+    //println!("{}", my_number2);
+
+
+
+
+
+
+
+
 }
--- src/option.rs
+++ src/option.rs
@@ -12,7 +12,7 @@ pub fn create_and_display_object() {
         println!("#1 {:?}", u);
     };
 
-    match (&user) {
+    match &user {
         Some(u) => {
             println!("#2 {:?}", u);
         }
