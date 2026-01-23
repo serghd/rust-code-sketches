0cc00812a5823a8f3473a0f1c87b69bba46d4264
serghd <serghd.box@gmail.com>
2026-01-23 19:20:47 +0200

    feat: Collections, add the sample for VecDeque

--- src/samples/collections.rs
+++ src/samples/collections.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::{BinaryHeap, HashMap, VecDeque};
 
 pub fn make_survey() {
     let data = vec![
@@ -21,24 +21,57 @@ pub fn make_survey() {
     }
 }
 
-fn remaining(input: &BinaryHeap<i32>) -> Vec<i32>{
+fn show_remaining_bheap(input: &BinaryHeap<i32>) -> Vec<i32> {
     let mut result = vec![];
     for item in input {
         result.push(*item);
     }
- result
+    result
 }
 
 pub fn binary_heap_demo() {
-    let data  = vec![12, 200, 1, 8, 34, 67, 9];
+    let data = vec![12, 200, 1, 8, 34, 67, 9];
     let mut b_heap = BinaryHeap::new();
     for item in data {
         b_heap.push(item);
     }
     while let Some(item) = b_heap.pop() {
-        println!("Current item: {}, items left: {:?}", item, remaining(&b_heap))
+        println!(
+            "Current item: {}, items left: {:?}",
+            item,
+            show_remaining_bheap(&b_heap)
+        )
     }
 }
 
+fn show_remaining_vdeque(my_deque: &VecDeque<(&str, bool)>) {
+    for item_pair in my_deque {
+        if item_pair.1 == false {
+            println!("You must: {}", item_pair.0);
+        }
+    }
+}
 
+fn done_last_job(my_deque: &mut VecDeque<(&str, bool)>) {
+    if let (Some(mut obj)) = my_deque.pop_back() {
+        obj.1 = true;
+        my_deque.push_front(obj);
+    }
+}
 
+pub fn vec_deque_demo() {
+    let jobs = vec!["do thing #1", "do thing #2", "do thing #3"];
+    let mut my_deque = VecDeque::new();
+    for job in jobs {
+        my_deque.push_back((job, false));
+    }
+
+    done_last_job(&mut my_deque);
+    done_last_job(&mut my_deque);
+
+    show_remaining_vdeque(&my_deque);
+
+    for item_pair in my_deque {
+        println!("{:?}", item_pair)
+    }
+}
